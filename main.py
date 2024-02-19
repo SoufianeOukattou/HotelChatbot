10df1d64a52e64d08823423db2f1da7653069edd
Soufiane Oukattou <117850230+SoufianeOukattou@users.noreply.github.com>
2024-02-19 20:19:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os.path
 import os
 import streamlit as st
 from streamlit_extras.add_vertical_space import add_vertical_space
-os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
 
 
 from llama_index.core import Settings
